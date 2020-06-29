35a1acc384e34f81b6fd15ced79e809ddc9ccde6
Pragyan Varshney <pragyanvarshney@gmail.com>
2020-06-29 12:55:01 +0530

    adding registration

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, session, request, redirect
 from flask_bootstrap import Bootstrap
+from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
+import yaml
+import os
 
 app = Flask(__name__)
 Bootstrap(app)
+
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
+
+app.config['SECRET_KEY'] = os.urandom(24)
+
 
 @app.route('/')
 def index():
@@ -18,10 +33,45 @@ def blogs(id):
 
 @app.route('/register/', methods = ['GET', 'POST'])
 def register():
+    if request.method == 'POST' :
+        userDetails = request.form
+        if userDetails['password'] != userDetails['confirm_password']:
+            flash('Passwords do not match! Try Again...','danger')
+            return render_template('register.html')
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO user(first_name, last_name, username, email, password)" "VALUES(%s,%s,%s,%s,%s)",(userDetails['first_name'],userDetails['last_name'],userDetails['username'],userDetails['email'],generate_password_hash(userDetails['password'])))
+        mysql.connection.commit()
+        cur.close()
+        flash('Registration successful! Please Login again', 'success')
+        return redirect('/login')
     return render_template('register.html')
+
+
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST' :
+        userDetails = request.form
+        username = userDetails['username']
+        cur = mysql.connection.cursor()
+        resultValue = cur.execute("SELECT * FROM user WHERE username = %s", ([username]))
+        if resultValue > 0:
+            user = cur.fetchone()
+            if(check_password_hash(user['password'], userDetails['password'])) :
+                session['login'] = True
+                session['firstName'] = user['first_name']
+                session['lastName'] = user['last_name']
+                flash('Welcome '+session['firstName']+'! You have successfully logged in.', 'success')
+            else:
+                cur.close()
+                flash('Password does not match', 'danger')
+                return render_template('login.html')
+    else:
+        cur.close()
+        flash('User not found', 'danger')
+        return render_template('login.html')
+    cur.close()
+    return redirect('/')
     return render_template('login.html')
 
 @app.route('/write-blog', methods=['GET', 'POST'])
